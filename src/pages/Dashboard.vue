8620bf98163450b850c490334bfd540eaa0baf22
Michael Nguyen <michaelnguyen@Michaels-MacBook-Pro.local>
2021-01-10 06:49:18 +0700

    add revenue table

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,43 +1,24 @@
 <template>
   <div>
-<base-button type="primary" v-on:click="searchCustomers">Search</base-button>
     <div class="row">
       <div class="col-lg-6 col-md-12" :class="{'text-right': isRTL}">
         <card type="chart">
           <template slot="header">
-            <h5 class="card-category">{{$t('dashboard.totalShipments')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> 763,215</h3>
+            <h5 class="card-category">{{$t('dashboard.totalRevenue')}}</h5>
+            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> {{totalRevenueStr}}</h3>
           </template>
           <div class="chart-area">
             <line-chart style="height: 100%"
                         v-if="loaded"
                         :chart-data="chartdata"
-                        :gradient-stops="purpleLineChart.gradientStops"
-                        :extra-options="purpleLineChart.extraOptions"
-                        chart-id="purple-line-chart">
+                        :gradient-stops="revenueLineChartOpts.gradientStops"
+                        :extra-options="revenueLineChartOpts.extraOptions"
+                        chart-id="revenue-line-chart">
             </line-chart>
           </div>
         </card>
       </div>
-      <div class="col-lg-6 col-md-12" :class="{'text-right': isRTL}">
-        <card type="chart">
-          <template slot="header">
-            <h5 class="card-category">{{$t('dashboard.dailySales')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-delivery-fast text-info "></i> 3,500€</h3>
-          </template>
-          <div class="chart-area">
-            <bar-chart style="height: 100%"
-                       chart-id="blue-bar-chart"
-                       :chart-data="blueBarChart.chartData"
-                       :gradient-stops="blueBarChart.gradientStops"
-                       :extra-options="blueBarChart.extraOptions">
-            </bar-chart>
-          </div>
-        </card>
-      </div>
       
-    </div>
-    <div class="row">
       <div class="col-lg-6 col-md-12" :class="{'text-right': isRTL}">
         <card type="chart">
           <template slot="header">
@@ -45,21 +26,20 @@
             <h3 class="card-title"><i class="tim-icons icon-send text-success "></i> 12,100K</h3>
           </template>
           <div class="chart-area">
-            <line-chart style="height: 100%"
-                        chart-id="green-line-chart"
-                        :chart-data="greenLineChart.chartData"
-                        :gradient-stops="greenLineChart.gradientStops"
-                        :extra-options="greenLineChart.extraOptions">
-            </line-chart>
+            <!-- chart here -->
           </div>
         </card>
       </div>
 
+    </div>
+    
+    <div class="row">
+      
       <div class="col-lg-6 col-md-12">
         <card class="card" :header-classes="{'text-right': isRTL}">
-          <h4 slot="header" class="card-title">{{$t('dashboard.simpleTable')}}</h4>
-          <div class="table-responsive">
-            <user-table></user-table>
+          <h4 slot="header" class="card-title">{{$t('dashboard.revenueTable')}}</h4>
+          <div class="table-responsive" style="max-height:480px;">
+            <revenue-table></revenue-table>
           </div>
         </card>
       </div>
@@ -70,7 +50,7 @@
   import LineChart from '@/components/Charts/LineChart';
   import BarChart from '@/components/Charts/BarChart';
   import * as chartConfigs from '@/components/Charts/config';
-  import UserTable from './Dashboard/UserTable';
+  import RevenueTable from './Dashboard/RevenueTable';
 
   import config from '@/config';
   import axios from "axios"
@@ -79,102 +59,46 @@
     components: {
       LineChart,
       BarChart,
-      UserTable,
+      RevenueTable,
     },
     data() {
       return {
         loaded: false,
         chartdata: null,
-        apiUrl: 'http://localhost:8080/apis/test',
-        purpleLineChart: {
-          dummyObj: {},
+        totalRevenueStr: '',
+
+        revenueLineChartOpts: {
           extraOptions: chartConfigs.purpleChartOptions,
-          key: "bac-dedad",
-          chartData: {
-            labels: ['JUL', 'AUG', 'Quan-3', 'OCT1', 'NOV1'],
-            datasets: [{
-              label: "Data",
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80],
-            }]
-          },
           gradientColors: config.colors.primaryGradient,
           gradientStops: [1, 0.2, 0],
         },
-        greenLineChart: {
-          extraOptions: chartConfigs.greenChartOptions,
-          chartData: {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-            datasets: [{
-              label: "My First dataset",
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80],
-            }]
-          },
-          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
-          gradientStops: [1, 0.4, 0],
-        },
-        blueBarChart: {
-          extraOptions: chartConfigs.barChartOptions,
-          chartData: {
-            labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-            datasets: [{
-              label: "Countries",
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45],
-            }]
-          },
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.4, 0],
-        }
       }
     },
     computed: {
-      enableRTL() {
-        return this.$route.query.enableRTL;
-      },
-      isRTL() {
+     isRTL() {
         return this.$rtl.isRTL;
-      },
-      bigLineChartCategories() {
-        return this.$t('dashboard.chartCategories');
       },
     },
     methods: {
-      searchCustomers(event) {
-        this.apiUrl = "http://change-api"
-
-        console.log(this.apiUrl)
-      },
       formatToMillion (labelValue) {
         return labelValue / 1000000
       },
+      formatPrice (labelValue) {
+        // Nine Zeroes for Billions
+        return Math.abs(Number(labelValue)) >= 1.0e+9
+    
+        ? Math.abs(Number(labelValue)) / 1.0e+9 + "B"
+        // Six Zeroes for Millions 
+        : Math.abs(Number(labelValue)) >= 1.0e+6
+    
+        ? Math.abs(Number(labelValue)) / 1.0e+6 + "M"
+        // Three Zeroes for Thousands
+        : Math.abs(Number(labelValue)) >= 1.0e+3
+    
+        ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
+    
+        : Math.abs(Number(labelValue));
+      }, 
     },
     async mounted() {
       this.i18n = this.$i18n;
@@ -184,9 +108,12 @@
         let result = await axios.get(`${config.api.url}/dashboard/revenueByMonth`);
 
         let priceData = [];
+        let totalRevenue = 0.0;
         result.data.data.forEach(e => {
+          totalRevenue += e;
           priceData.push(this.formatToMillion(e))
-        })
+        });
+        this.totalRevenueStr = this.formatPrice(totalRevenue);
 
         this.chartdata = {
           labels: result.data.labels,
---
+++ src/pages/Dashboard/RevenueTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <base-table :data="revenueTable.data"
+              :columns="revenueTable.columns"
+              thead-classes="text-primary">
+  </base-table>
+</template>
+<script>
+  import { BaseTable } from "@/components";
+  import config from '@/config';
+  import axios from "axios";
+
+  export default {
+    components: {
+      BaseTable
+    },
+    data() {
+      return {
+        revenueTable: {
+          columns: ["District", "Total"] ,
+          data: [],
+        }
+      }
+    },
+    computed: {
+    },
+    methods: {
+      formatPrice(price) {
+        if (price && price > 0) {
+          return  price.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+        }
+        return price;
+      }
+    },
+    mounted() {
+      axios
+      .get(`${config.api.url}/dashboard/revenueByDistrict`)
+      .then(response => {
+        if(response.data.data) {
+          let results = [];
+          response.data.data.forEach(element => {
+            results.push({district: element.District, total: this.formatPrice(element.Total)})
+          });
+
+          this.revenueTable.data = results;
+        }
+      })
+    },
+  }
+</script>
+<style>
+</style>
