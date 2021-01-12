02833c596971f5650bc533587fa46eadcce79bc2
Michael Nguyen <michaelnguyen@Michaels-MacBook-Pro.local>
2021-01-12 08:43:39 +0700

    cleanup code

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,11 +5,6 @@ import NotFound from "@/pages/NotFoundPage.vue";
 // Admin pages
 const Dashboard = () => import(/* webpackChunkName: "dashboard" */"@/pages/Dashboard.vue");
 const Profile = () => import(/* webpackChunkName: "common" */ "@/pages/Profile.vue");
-const Notifications = () => import(/* webpackChunkName: "common" */"@/pages/Notifications.vue");
-const Icons = () => import(/* webpackChunkName: "common" */ "@/pages/Icons.vue");
-const Maps = () => import(/* webpackChunkName: "common" */ "@/pages/Maps.vue");
-const Typography = () => import(/* webpackChunkName: "common" */ "@/pages/Typography.vue");
-const TableList = () => import(/* webpackChunkName: "common" */ "@/pages/TableList.vue");
 const SearchCustomers = () => import(/* webpackChunkName: "common" */ "@/pages/SearchCustomers.vue");
 
 
@@ -25,40 +20,15 @@ const routes = [
         component: Dashboard
       },
       {
+        path: "search-customers",
+        name: "search customers",
+        component: SearchCustomers
+      },
+      {
         path: "profile",
         name: "profile",
         component: Profile
       },
-      {
-        path: "notifications",
-        name: "notifications",
-        component: Notifications
-      },
-      {
-        path: "icons",
-        name: "icons",
-        component: Icons
-      },
-      {
-        path: "maps",
-        name: "maps",
-        component: Maps
-      },
-      {
-        path: "typography",
-        name: "typography",
-        component: Typography
-      },
-      {
-        path: "table-list",
-        name: "table-list",
-        component: TableList
-      },
-      {
-        path: "search-customers",
-        name: "search customers",
-        component: SearchCustomers
-      }
     ]
   },
   { path: "*", component: NotFound },
