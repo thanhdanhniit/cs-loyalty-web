e87b5816c2bb648efc58e77bbbb3d36cff1fb747
Michael Nguyen <michaelnguyen@Michaels-MacBook-Pro.local>
2021-01-12 17:15:52 +0700

    deployment script

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,8 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cs-loyalty-web/'
+    : '/'
 };
