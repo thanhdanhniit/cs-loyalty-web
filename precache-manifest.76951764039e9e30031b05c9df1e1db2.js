d46e8a934168ae40f16d542d54adf98172049d78
Michael Nguyen <michaelnguyen@Michaels-MacBook-Pro.local>
2021-01-14 12:16:12 +0700

    deploy

---
+++ precache-manifest.76951764039e9e30031b05c9df1e1db2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afbfaa2a64cebfd0077f73abe15659",
+    "url": "/cs-loyalty-web/apple-icon.png"
+  },
+  {
+    "revision": "d614630a6ea5a4703049",
+    "url": "/cs-loyalty-web/css/app.ea5ef3e0.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/cs-loyalty-web/favicon.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/cs-loyalty-web/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/cs-loyalty-web/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/cs-loyalty-web/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/cs-loyalty-web/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "3b91359dcf1cf96c8440a7532ac61dda",
+    "url": "/cs-loyalty-web/img/anime3.png"
+  },
+  {
+    "revision": "2a5a031c63d290c6cd3803e21d8c3b5d",
+    "url": "/cs-loyalty-web/img/anime6.png"
+  },
+  {
+    "revision": "d2caaad36a395104bd99f30487798d79",
+    "url": "/cs-loyalty-web/img/apple-icon.png"
+  },
+  {
+    "revision": "9b0c49b657dd738a77a37e7dabcc0b5d",
+    "url": "/cs-loyalty-web/img/bg5.jpg"
+  },
+  {
+    "revision": "d25412bcd9af1fb98017c8144a4e40d1",
+    "url": "/cs-loyalty-web/img/default-avatar.png"
+  },
+  {
+    "revision": "fadc3f3a4c94b151d8b8ed33456c2344",
+    "url": "/cs-loyalty-web/img/emilyz.jpg"
+  },
+  {
+    "revision": "4422794dfa779751326660ae681b082e",
+    "url": "/cs-loyalty-web/img/favicon.png"
+  },
+  {
+    "revision": "f220d1815d421bfa192ee352780ba194",
+    "url": "/cs-loyalty-web/img/img_3115.jpg"
+  },
+  {
+    "revision": "40fdb6e1e72e21ff904da682c46d0116",
+    "url": "/cs-loyalty-web/img/james.jpg"
+  },
+  {
+    "revision": "93f1328bd264f02aacef8b0c6b498445",
+    "url": "/cs-loyalty-web/img/mike.jpg"
+  },
+  {
+    "revision": "a219d0c3d5bae43ade2ddaa296cea98f",
+    "url": "/cs-loyalty-web/index.html"
+  },
+  {
+    "revision": "d614630a6ea5a4703049",
+    "url": "/cs-loyalty-web/js/app.d1e3b5bc.js"
+  },
+  {
+    "revision": "2b00bfca853d4e03b073",
+    "url": "/cs-loyalty-web/js/chunk-vendors.c598e2a1.js"
+  },
+  {
+    "revision": "98a7bdd8a63a93756c12",
+    "url": "/cs-loyalty-web/js/common.d1532532.js"
+  },
+  {
+    "revision": "cea7204a4990483b1e2e",
+    "url": "/cs-loyalty-web/js/common~dashboard.8d54f026.js"
+  },
+  {
+    "revision": "182b5187e3c47a32f973",
+    "url": "/cs-loyalty-web/js/dashboard.2070709f.js"
+  },
+  {
+    "revision": "1a167cfd82a1a7480ea601fccc8d7e18",
+    "url": "/cs-loyalty-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cs-loyalty-web/robots.txt"
+  }
+]);
